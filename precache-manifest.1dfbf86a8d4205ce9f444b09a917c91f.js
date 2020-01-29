b1062e056230d54466824245bc5fd355fb05852d
msheffield <msheffield@adaptivebiotech.com>
2020-01-29 10:15:12 -0800

    Updates

---
+++ precache-manifest.1dfbf86a8d4205ce9f444b09a917c91f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343358a8810b9380fb901b5f4029326b",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "a11faba1b1acf0245c1f",
+    "url": "/clicky-game/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "096096a05405cff2a786",
+    "url": "/clicky-game/static/js/2.3b555bf2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.3b555bf2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a11faba1b1acf0245c1f",
+    "url": "/clicky-game/static/js/main.152605f5.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  }
+]);
